c3d9a893e75c2901ec521c7a1515a7a754d7d2bd
MuhammadHammadAhmed <42751169+MuhammadHammadAhmed@users.noreply.github.com>
2022-06-16 10:42:54 +0500

    integrated clients and recovery screen

--- MobileApp/app/components/screen/ClientScreen.js
+++ MobileApp/app/components/screen/ClientScreen.js
@@ -1,65 +1,65 @@
-import {React,useEffect,useState} from 'react';
+import { React, useEffect, useState } from 'react';
 import { Image, Button, Text, FlatList, View, StyleSheet, TextInput } from 'react-native';
 import Card from '../Card';
 import NewCard from '../NewCard';
 import colors from '../colors';
 import Screen from '../Screen'
-import {getClients} from '../APIcalls/getRequests'
+import { getClients } from '../APIcalls/getRequests'
 
 
 function ClientScreen({ navigation }) {
 
   const list = [
-    { Name: 'Hassan Mansoor1', PhoneNumber:'0300-xxxxxxx', Amount:'xxxxxxx', value: 1 },
-    { Name: 'Hammad Ahmed', PhoneNumber:'0300-xxxxxxx', Amount:'xxxxxxx', value: 2 },
-    { Name: 'Anas Mansoor', PhoneNumber:'0300-xxxxxxx', Amount:'xxxxxxx', value: 3 },
-    { Name: 'Bilal Zia', PhoneNumber:'0300-xxxxxxx', Amount:'xxxxxxx', value: 4 },
-    { Name: 'Mohib Zia', PhoneNumber:'0300-xxxxxxx', Amount:'xxxxxxx', value: 5 },
+    { Name: 'Hassan Mansoor1', PhoneNumber: '0300-xxxxxxx', Amount: 'xxxxxxx', value: 1 },
+    { Name: 'Hammad Ahmed', PhoneNumber: '0300-xxxxxxx', Amount: 'xxxxxxx', value: 2 },
+    { Name: 'Anas Mansoor', PhoneNumber: '0300-xxxxxxx', Amount: 'xxxxxxx', value: 3 },
+    { Name: 'Bilal Zia', PhoneNumber: '0300-xxxxxxx', Amount: 'xxxxxxx', value: 4 },
+    { Name: 'Mohib Zia', PhoneNumber: '0300-xxxxxxx', Amount: 'xxxxxxx', value: 5 },
   ];
   // console.log("in client Screen",list)
-  const[clients,setClients]=useState();
+  const [clients, setClients] = useState();
   useEffect(() => {
     fetch("https://paym-api.herokuapp.com/ClientData")
-    .then((response) => response.json())
-    .then((responseJson) => {
-        console.log("in getAPI",responseJson);
-         setClients(responseJson.Data);
-  
-    })
-    .catch((error) => {
-    console.error(error);
-    });
-   
-   
-     },[]);
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log("ClientScreen in getAPI", responseJson);
+        setClients(responseJson.Data);
 
-    return (
-        <Screen>
-            <View style={styles.logoContainer}>
-                <Image
-                    style={styles.logo} 
-                    source={require('../../assets/logo.png')} 
-                />
-                <View style={styles.textContainer}>
-                  <TextInput style={{fontWeight:'bold', width: '100%'}} placeholder='Client Name'/>
-                </View>
-                <Button title='Search'/>
-            </View>
-            {clients!=null?
-            <FlatList 
-                    data={clients}
-                    keyExtractor={listing => listing.ClientId}
-                    renderItem={({item}) =>
-                    <Card 
-                      title={item.ClientName}
-                 
-                      subTitle={item.PhoneNumber}
-                            subSubTitle={item.Amount}
-                            onPress={()=>navigation.navigate('RecoveryScreen', item)} 
-                        /> 
-                    } 
-                />:<></>}
-                {/* <NewCard
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+
+  }, []);
+
+  return (
+    <Screen>
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require('../../assets/logo.png')}
+        />
+        <View style={styles.textContainer}>
+          <TextInput style={{ fontWeight: 'bold', width: '100%' }} placeholder='Client Name' />
+        </View>
+        <Button title='Search' />
+      </View>
+      {clients != null ?
+        <FlatList
+          data={clients}
+          keyExtractor={listing => listing.ClientId}
+          renderItem={({ item, i }) =>
+            <Card
+              key={i}
+              title={item.ClientName}
+              subTitle={item.ClientPhoneNumber}
+              subSubTitle={item.Amount}
+              onPress={() => navigation.navigate('RecoveryScreen', item)}
+            />
+          }
+        /> : <></>}
+      {/* <NewCard
                   items={categories[0]}
                   onPress={()=> navigation.navigate('RecoveryScreen', categories[0])
                   }
@@ -84,29 +84,29 @@ function ClientScreen({ navigation }) {
                   onPress={()=> navigation.navigate('RecoveryScreen', categories[4])
                   }
                 /> */}
-        </Screen>
-    );
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
-    logo:{
-        height: 120,
-        width: 300,
-      },
-      logoContainer:{ 
-        alignItems: 'center',
-        width: '100%', 
-        marginVertical: 10,
-        // backgroundColor: colors.
-      },
-      textContainer:{
-        width: '80%',
-        height: 50,
-        backgroundColor: colors.light,
-        borderRadius: 15,
-        padding: 10,
-        marginBottom: 10,
-      },
+  logo: {
+    height: 120,
+    width: 300,
+  },
+  logoContainer: {
+    alignItems: 'center',
+    width: '100%',
+    marginVertical: 10,
+    // backgroundColor: colors.
+  },
+  textContainer: {
+    width: '80%',
+    height: 50,
+    backgroundColor: colors.light,
+    borderRadius: 15,
+    padding: 10,
+    marginBottom: 10,
+  },
 })
 
 export default ClientScreen;
--- MobileApp/app/components/screen/RecoveryScreen.js
+++ MobileApp/app/components/screen/RecoveryScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Image, StyleSheet, Switch, TouchableOpacity, View, ScrollView} from 'react-native';
+import { Image, StyleSheet, Switch, TouchableOpacity, View, ScrollView } from 'react-native';
 
 import Screen from '../Screen';
 import Icon from '../Icon';
@@ -10,141 +10,197 @@ import CameraInput from '../CameraInput';
 import colors from '../colors';
 import AppButton from '../AppButton';
 import AppText from '../AppText';
-import {postRequest} from '../APIcalls/postRequests'
+import { postRequest } from '../APIcalls/postRequests'
+import axios from 'axios';
 
 
 
 
 function RecoveryScreen({ navigation, route }) {
 
-  const listing = route.params;
-
   const [isNew, setIsNew] = useState(false);
   const [imageUri, setImageUri] = useState(null);
-  const [textInput, setTextInput] = useState(null);
+  const [textInput, setTextInput] = useState("");
+  const [Img, setImage] = useState("");
+
+
+  const listing = route.params;
+
+  const PaymentId = listing.ClientId.toString()
+  const PaymentName = listing.ClientName
+  const PaymentNumber = listing.ClientPhoneNumber
+  const PaymentEmail = listing.ClientEmail
+  // const Paymentamount = textInput
+  // const imageUrl = Img
+  console.log(PaymentNumber, PaymentName, "PaymentID");
+
+
 
   const handlePress = () => {
-    console.log(textInput);
   }
 
-  const handleContinue =async () => {
-    
-    console.log(listing);
-    let api= "Clientdata";
-    let payload={
-      ClientId:"122",
-clientName:"Wajahat",
-clientEmail:"wajee@pacific.com.pk",
-clientAmount:"190",
-  status:"false"
+  const handleContinue = async () => {
 
-    }
-  //  let resp= await postRequest(api,payload);
-    console.log("image",imageUri);
-  //  navigation.navigate('OTPScreen', {listing, isNew, textInput})
+    console.log(Img, "ImgUrl");
+    axios({
+      method: 'post',
+      url: 'https://paym-api.herokuapp.com/PaymentData',
+      data: {
+
+        PaymentId: PaymentId,
+        PaymentName: PaymentName,
+        PaymentNumber: PaymentNumber,
+        PaymentEmail: PaymentEmail,
+        PaymentAmount: textInput,
+        imageUrl: Img,
+        status: "false"
+
+      }, withCredentials: true
+    })
+      .then((response) => {
+        console.log(JSON.stringify(response.data), "response");
+        alert("Send Otp for Check Email")
+
+      })
+      .catch((error) => {
+        console.log(error, "error");
+      })
+
+
   }
+
+
+  if (imageUri === null) {
+    console.log("not image");
+  } else {
+
+    var formData = new FormData
+    formData.append("image", {
+      uri: imageUri,
+      type: "image/jpg",
+      name: "photo.jpg"
+    })
+    axios({
+      method: 'post',
+      url: "https://paym-api.herokuapp.com/upload",
+      data: formData,
+      headers: { 'Content-Type': 'multipart/form-data' }
+    })
+      .then(res => {
+        // console.log(JSON.stringify(res.data.ImageUrl), "res");
+        setImage(res.data.ImageUrl)
+      })
+      .catch(err => { console.log(err, "error"); })
+  }
+
+
+
+
+  // console.log(Img,"ImgImgImgImg");
+
   return (
     <Screen style={styles.backGround}>
       <View style={styles.logoContainer}>
-      <Image
-          style={{width: 330, height: 140}} 
-          source={require('../../assets/logo.png')} 
+        <Image
+          style={{ width: 330, height: 140 }}
+          source={require('../../assets/logo.png')}
         />
       </View>
       <ScrollView>
-      <View style={styles.inputContainer}>
-        <AppText style={styles.name}>{listing.ClientName}</AppText>
-        <AppTextInput
-          placeholder='Amount'
-          icon='currency-rupee'
-          keyboardType='numeric'
-          onChangeText={(textInput) => setTextInput(textInput)}
-        />
-      </View>
-      <View style={ styles.chequeBarContainer}>
-        <View style={styles.chequeBarLeft}>
-          <AppText style={styles.text}>Cheque</AppText>
-        </View>
-        <View style={styles.chequeBarRight}>
-          <Switch 
-            value={isNew} 
-            onValueChange={newValue => setIsNew(newValue)}
+        <View style={styles.inputContainer}>
+          <AppText style={styles.name}>{listing.ClientName}</AppText>
+          <AppTextInput
+            placeholder='Amount'
+            icon='currency-rupee'
+            keyboardType='numeric'
+            onChangeText={(textInput) => setTextInput(textInput)}
           />
         </View>
-      </View>
-      <View style={styles.image}>
-        <CameraInput 
-          imageUri={imageUri}
-          onChangeImage={uri => setImageUri(uri)} 
-        />
+        <View style={styles.chequeBarContainer}>
+          <View style={styles.chequeBarLeft}>
+            <AppText style={styles.text}>Cheque</AppText>
+          </View>
+          <View style={styles.chequeBarRight}>
+            <Switch
+              value={isNew}
+              onValueChange={newValue => setIsNew(newValue)}
+            />
+          </View>
+        </View>
+        <View style={styles.image}>
+          <CameraInput
+            imageUri={imageUri}
+            onChangeImage={uri => setImageUri(uri)
+            }
+          />
 
-        <AppButton title='Continue' color= 'royalBlue' onPress={()=> handleContinue()}/>
-      </View>
-      <View style={styles.iconBar}>
-          <Icon name='home' backgroundColor={colors.backGround} iconColor= {colors.royalBlue} onPress={handlePress}/>
+          <AppButton title='Continue' color='royalBlue' onPress={() => handleContinue()} />
+        </View>
+        <View style={styles.iconBar}>
+          <Icon name='home' backgroundColor={colors.backGround} iconColor={colors.royalBlue} onPress={handlePress} />
           <Icon name='account' backgroundColor={colors.backGround} iconColor={colors.royalBlue} />
-          <Icon name='magnify' backgroundColor={colors.backGround} iconColor= {colors.royalBlue} />
-      </View>
+          <Icon name='magnify' backgroundColor={colors.backGround} iconColor={colors.royalBlue} />
+        </View>
       </ScrollView>
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
-    backGround: {
-      backgroundColor: '#D6DCE5',
-      flex: 1,
-    },
-    chequeBarContainer:{
-      width: '100%', 
-      flexDirection: 'row',
-      marginBottom: 15,
-    },
-    chequeBarRight:{
-      width: '50%',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    chequeBarLeft:{
-      width: '50%',
-      justifyContent: 'center',
-    },
-    iconBar:{
-      marginVertical: 15,
-      flexDirection: 'row',
-      width: '100%',
-      alignItems: 'center',
-      justifyContent: 'space-evenly',
-    },
-    image:{
-      alignItems: 'center',
-      marginVertical: 10,
-    },
-    inputContainer:{
-      width: '100%', 
-      paddingHorizontal: 20,
-    },
-    logo:{
-      height: 120,
-      width: 300,
-    },
-    logoContainer:{ 
-      alignItems: 'center',
-      width: '100%', 
-      marginVertical: 10,
-    },
-    name:{
-      fontWeight: '900',
-      fontSize: 20,
-      fontStyle: 'italic',
-      color: colors.primary,
-    },
-    text:{
-      fontWeight: '900',
-      fontStyle: 'italic',
-      textAlign: 'right',
-    },
-  });
+  backGround: {
+    backgroundColor: '#D6DCE5',
+    flex: 1,
+  },
+  chequeBarContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    marginBottom: 15,
+  },
+  chequeBarRight: {
+    width: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  chequeBarLeft: {
+    width: '50%',
+    justifyContent: 'center',
+  },
+  iconBar: {
+    marginVertical: 15,
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  image: {
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+  logo: {
+    height: 120,
+    width: 300,
+  },
+  logoContainer: {
+    alignItems: 'center',
+    width: '100%',
+    marginVertical: 10,
+  },
+  name: {
+    fontWeight: '900',
+    fontSize: 20,
+    fontStyle: 'italic',
+    color: colors.primary,
+  },
+  text: {
+    fontWeight: '900',
+    fontStyle: 'italic',
+    textAlign: 'right',
+  },
+});
 
 
 export default RecoveryScreen;
