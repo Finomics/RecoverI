0f3c4738d447676113f32be82ddcea26cd3cdd55
Raza <Razaattari2468@gmail.com>
2022-07-14 00:50:35 +0500

    Add Login Register, Admin Register & Global State

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { StyleSheet } from 'react-native';
@@ -12,23 +12,32 @@ import AuthNavigator from './app/components/navigation/AuthNavigator';
 import AddMemberScreen from './app/components/screen/AddMemberScreen';
 import RegisterScreen from './app/components/screen/RegisterScreen';
 import LoginScreen from './app/components/screen/LoginScreen';
+import { StoreProvider } from './app/components/screen/GlobalState';
+
+
 
 export default function App() {
+  const [Role, setRole] = useState([])
+  console.log(Role, "Set GolbalState Data");
+
   return (
     // <Screen>
     // <AddMemberScreen/>
     //   <ClientScreen/>
 
-   //  <RegisterScreen/> 
-//   <LoginScreen/> 
+    //  <RegisterScreen/> 
+    //   <LoginScreen/> 
+    <StoreProvider value={{ Role, setRole }}>
 
-    <NavigationContainer>
-      <AuthNavigator/>
-    </NavigationContainer>
+      <NavigationContainer>
+        <AuthNavigator />
+      </NavigationContainer>
+
+    </StoreProvider>
   );
 }
 const styles = StyleSheet.create({
-  container:{
+  container: {
     justifyContent: 'center',
     alignItems: 'center',
   },
