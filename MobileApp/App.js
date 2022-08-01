8120b3fb37e8f6929409e35020d6f8d8695bc067
MuhammadHammadAhmed <42751169+MuhammadHammadAhmed@users.noreply.github.com>
2022-08-01 15:31:38 +0500

    functional app with some fixtures required

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -41,7 +41,7 @@ export default function App() {
     // <RiderHomeScreen/>
   //  <TransferScreen/>
     // <OTPScreen/>
-    
+    //
     // <TopButtons/>
 
     <StoreProvider value={{ Role, setRole }}>
