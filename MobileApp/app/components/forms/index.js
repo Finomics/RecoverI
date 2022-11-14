a35060da331f7c180c3b27508f8b5c8e46ed5d93
Hassan-1995 <63869278+Hassan-1995@users.noreply.github.com>
2022-11-14 21:47:13 +0500

    Phone Box

--- MobileApp/app/components/forms/index.js
+++ MobileApp/app/components/forms/index.js
@@ -1,6 +1,7 @@
 export { default as AppForm } from './AppForm';
 export { default as AppFormField } from './AppFormField';
 export { default as AppFormPassword } from './AppFormPassword';
+export { default as AppFormPhone } from './AppFormPhone'
 export { default as ErrorMessage } from './ErrorMessage';
 export { default as SubmitButton } from './SubmitButton';
 export { default as AppFormPicker } from './AppFormPicker';
