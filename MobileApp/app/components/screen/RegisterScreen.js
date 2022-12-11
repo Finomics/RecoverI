7a3c4bd4e6115b9e30b7085022b00ea17b3bbb74
Hassan-1995 <63869278+Hassan-1995@users.noreply.github.com>
2022-12-11 20:08:10 +0500

    remove email verification

--- MobileApp/app/components/screen/RegisterScreen.js
+++ MobileApp/app/components/screen/RegisterScreen.js
@@ -15,7 +15,7 @@ import { Url } from './Core';
 
 const validationSchema = Yup.object().shape({
     name: Yup.string().required().label("Name"),
-    email: Yup.string().required().email().label("Email"),
+    email: Yup.string().email().label("Email"),
     password: Yup.string().required().min(4).label("Password"),
 });
 
